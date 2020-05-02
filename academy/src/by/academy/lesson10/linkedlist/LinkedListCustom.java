91e9839c9f519e90365a0964c82558c9ec3bdec2
dmitrysc <dmitrysc@t480-SchepinD.playtika.local>
2020-05-02 14:08:35 +0300

    linked list

--- academy/src/by/academy/lesson10/linkedlist/LinkedListCustom.java
+++ academy/src/by/academy/lesson10/linkedlist/LinkedListCustom.java
@@ -2,16 +2,75 @@ package by.academy.lesson10.linkedlist;
 
 public class LinkedListCustom<T> {
 
-	public int size;
+	private Node tail;
+	private Node head;
+
+	private int size;
 
 	public int size() {
 		return size;
 	}
 
-//	class Node<T> {
-//		Node<T> next;
-//		Node<T> prev;
-//		T value;
-//	}
+	class Node {
+
+		Node next;
+		Node prev;
+		T value;
+
+		public Node(Node prev, Node next, T newVal) {
+			this.prev = prev;
+			this.next = next;
+			this.value = newVal;
+		}
+	}
+
+	public void add(int index, T newVal) {
+		Node current = head;
+		int counter = 0;
+		while (current != null) {
+			counter++;
+			if (counter == index) {
+				Node newNode = new Node(current.prev, current, newVal);
+				Node prev = current.prev;
+				prev.next = newNode;
+				current.prev = newNode;
+				return;
+			}
+			current = current.next;
+		}
+	}
+
+	public T remove(int index) {
+		Node current = head;
+		int counter = 0;
+		while (current != null) {
+			counter++;
+			if (counter == index) {
+				T val = current.value;
+				Node prev = current.prev;
+				Node next = current.next;
+				prev.next = next;
+				next.prev = prev;
+				return val;
+			}
+			current = current.next;
+		}
+		// throw exception
+		return null;
+	}
+
+	public T get(int index) {
+		Node current = head;
+		int counter = 0;
+		while (current != null) {
+			counter++;
+			if (counter == index) {
+				return current.value;
+			}
+			current = current.next;
+		}
+		// throw exception
+		return null;
+	}
 
 }
