02f0ffefca803e819d3c602276abe97e897edc53
dmitrysc <dmitrysc@t480-SchepinD.playtika.local>
2020-09-14 17:33:32 +0300

    tasks

---
+++ academy/src/by/academy/lesson18/threads/sleep/SleepRunnableDemo.java
@@ -0,0 +1,18 @@
+package by.academy.lesson18.threads.sleep;
+
+public class SleepRunnableDemo {
+	public static void main(String[] args) {
+		SleepRunnable sleepRunnable = new SleepRunnable();
+
+		Thread one = new Thread(sleepRunnable);
+		one.setName("Fred");
+		Thread two = new Thread(sleepRunnable);
+		two.setName("Lucy");
+		Thread three = new Thread(sleepRunnable);
+		three.setName("Ricky");
+
+		one.start();
+		two.start();
+		three.start();
+	}
+}
